321e08ebc6a7e30cf72f053aba3df3ecc9f204db
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-26 08:58:03 +0200

    Add first counter

---
+++ components/Counter.js
@@ -0,0 +1,40 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('h1', {}, this.state.counter),
+            React.createElement('button', {onClick: this.increment, className: 'btn'}, '+'),
+            React.createElement('button', {onClick: this.decrement, className: 'btn'}, '-'),
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+
+// decrement: function() {
+//         this.setState({
+//             counter: this.state.counter - 1
+//         });
+//     },
+
+//     React.createElement('button', {onClick: this.decrement},
+//             React.createElement('span', {}, this.state.counter)
+//             );
