54f3e66d723e5e04cfa3e2c93954d38c3f3e10c4
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-26 12:52:56 +0200

    Finish task-14-6

--- components/Counter.js
+++ components/Counter.js
@@ -5,6 +5,29 @@ var Counter = React.createClass({
         };
     },
 
+    componentDidMount: function() {
+        console.log("Component " + this.state.counter + " did mount");
+    },
+
+    shouldComponentUpdate: function(nextProps, nextState) {
+        console.log("Should component " + this.state.counter + " update",
+            nextState,
+            nextProps
+            );
+        return true;
+    },
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("Component " + this.state.counter + " did update",
+            prevProps,
+            prevState
+            );
+    },
+
+    componentWillUnmount: function() {
+        console.log('Component will unmount');
+    },
+
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
@@ -28,13 +51,5 @@ var Counter = React.createClass({
 
 var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
-
-// decrement: function() {
-//         this.setState({
-//             counter: this.state.counter - 1
-//         });
-//     },
-
-//     React.createElement('button', {onClick: this.decrement},
-//             React.createElement('span', {}, this.state.counter)
-//             );
+ReactDOM.render(element, document.getElementById('app-2'));
+ReactDOM.render(element, document.getElementById('app-3'));
